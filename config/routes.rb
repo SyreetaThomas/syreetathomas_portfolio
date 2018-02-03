eee39133902040624642ab2cb5fc78da45d52a98
Syreeta Thomas <syreeta.thomas@gmail.com>
2018-02-02 23:24:40 -0600

    Changes custom routes for pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :portfolios
 
-  get 'about', to: 'pages#about'
+  get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
 
   resources :blogs
